c66efb96ebe0be9a3678824c70f48a6913bf8eba
Daniel Farré Serra <danifarre@users.noreply.github.com>
2020-06-15 12:12:21 +0200

    First commit

---
+++ mountain_car.py
@@ -0,0 +1,59 @@
+import numpy as np
+import gym
+import q_learner as ql
+
+MAX_NUM_EPISODES = 10
+
+
+def train(agent, environment):
+    best_reward = -float('inf')
+
+    for episode in range(MAX_NUM_EPISODES):
+        done = False
+        obs = environment.reset()
+        total_reward = 0.0
+
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, done, info = environment.step(action)
+            agent.learn(obs, action, reward, next_obs)
+            obs = next_obs
+            total_reward += reward
+
+        if total_reward > best_reward:
+            best_reward = total_reward
+
+        print("EPisodio número {} con recompensa: {}, mejor recompensa: {}, epsilon: {}".format(episode, total_reward,
+                                                                                                best_reward,
+                                                                                                agent.epsilon))
+
+    return np.argmax(agent.q, axis=2)
+
+
+def test(agent, environment, policy):
+    done = False
+    obs = environment.reset()
+    total_reward = 0.0  # Recompensa total de cada episodio
+
+    while not done:
+        action = policy[agent.discretize(obs)]
+        next_obs, reward, done, info = environment.step(action)
+        obs = next_obs
+        total_reward += reward
+
+    return total_reward
+
+
+if __name__ == "__main__":
+    environment = gym.make("MountainCar-v0")
+    agent = ql.QLearner(environment)
+
+    learned_policy = train(agent, environment)
+
+    monitor_path = "./monitor_output"
+    environment = gym.wrappers.Monitor(environment, monitor_path, force=True)
+
+    for _ in range(1000):
+        test(agent, environment, learned_policy)
+
+    environment.close()
---
+++ q_learner.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+EPSILON_MIN = 0.005  # Aprendemos mientras el incremento de aprendizaje sea superior a dicho valor
+MAX_NUM_EPISODES = 10
+STEPS_PER_EPISODE = 200
+MAX_NUM_STEPS = MAX_NUM_EPISODES * STEPS_PER_EPISODE
+EPSILON_DECAY = 500 * EPSILON_MIN / MAX_NUM_STEPS  # Caida de epsilon en cada paso
+ALPHA = 0.05  # Ratio de aprendizaje del agente
+GAMMA = 0.98  # Factor de descuento
+NUM_DISCRETE_BINS = 30
+EPSILON = 1.00
+
+
+class QLearner(object):
+
+    def __init__(self, environment):
+        self.obs_shape = environment.observation_space.shape
+        self.obs_high = environment.observation_space.high
+        self.obs_low = environment.observation_space.low
+        self.obs_bins = NUM_DISCRETE_BINS
+        self.bin_width = (self.obs_high - self.obs_low) / self.obs_bins
+
+        self.action_shape = environment.action_space.n
+        self.q = np.zeros((self.obs_bins, self.obs_bins, self.action_shape))
+
+        self.alpha = ALPHA
+        self.gamma = GAMMA
+        self.epsilon = EPSILON
+
+    def discretize(self, obs):
+        return tuple(((obs - self.obs_low) / self.bin_width).astype(int))
+
+    def get_action(self, obs):
+        discrete_obs = self.discretize(obs)
+
+        # Epsilon-Greedy
+        if self.epsilon > EPSILON_MIN:
+            self.epsilon -= EPSILON_DECAY
+
+        if np.random.random() > self.epsilon:
+            return np.argmax(self.q[discrete_obs])
+        else:
+            return np.random.choice([action for action in range(self.action_shape)])
+
+    def learn(self, obs, action, reward, next_obs):
+        discrete_obs = self.discretize(obs)
+        discrete_next_obs = self.discretize(next_obs)
+        td_target = reward + self.gamma * np.max(self.q[discrete_next_obs])
+        td_error = td_target - self.q[discrete_obs][action]
+        self.q[discrete_obs][action] += self.alpha * td_error
